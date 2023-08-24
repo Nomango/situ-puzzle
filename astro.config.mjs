d506f25a0df4f40715e4f1d84f17677c90c31c1f
Haibo <nomango@qq.com>
2023-08-24 15:02:58 +0800

    build: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // astro.config.ts
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel(),
   integrations: [
     UnoCSS({
       injectReset: true, // or a path to the reset file
